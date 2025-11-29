a7891055264d1633d50649b585c8879953870500
svictornmanuel <svictornmanuel@gmail.com>
2025-11-29 14:02:49 -0500

    Add files via upload

---
+++ GreenPaths/js/leaflet.js
@@ -0,0 +1,6 @@
+var map = L.map('map').setView([4.60971, -74.08175], 11);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
