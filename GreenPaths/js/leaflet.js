bdd9e82624b40e3785ec96b8889f4b8565f14987
svictornmanuel <svictornmanuel@gmail.com>
2025-12-12 19:58:33 -0500

    Eliminación de archivos innecesarios (leaflet.js)

--- GreenPaths/js/leaflet.js
+++
@@ -1,6 +0,0 @@
-var map = L.map('map').setView([4.60971, -74.08175], 11);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
